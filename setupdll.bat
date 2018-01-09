40f1c27e00e7c8132fb2d3cb9a2eadfcbc304166
Committer N <anon_n@example.com>
2018-01-09 13:11:26 -0800

    edited setup2.bat, renamed to setupdll.bat.

---
+++ setupdll.bat
@@ -0,0 +1,46 @@
+:: This sets the path for building rm5dll/racermate/dor.bat
+
+::set vim=%HOMEDRIVE%%HOMEPATH%\vim
+::set vim=c:\cygwin64\bin\vim
+
+::set HOME=%HOMEDRIVE%%HOMEPATH%\MKSNT
+::set JAVA_HOME=C:\Program Files (x86)\java\jkd1.7.0_10
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_25
+set CLASSPATH=.;C:\Program Files\Java\jdk1.8.0_25
+::set jdkhome =.;C:\Program Files\Java\jdk1.8.0_25
+::set ROOTDIR=c:
+::set TMP=%HOMEDRIVE%%HOMEPATH%\TEMP
+::set TMPDIR=%HOMEDRIVE%%HOMEPATH%\TEMP
+::set LOGNAME=mks
+::set GLOB=%HOMEDRIVE%%HOMEPATH%\mksnt\etc\glob.exe
+
+set CYGWIN=nodosfilewarning
+
+PATH = C:\Program Files (x86)\Vim\vim74
+PATH = %PATH%;c:\cygwin64\bin
+::PATH = %PATH%;%HOMEDRIVE%%HOMEPATH%\utils
+::PATH = %PATH%;c:\Program Files (x86)\java\jkd1.7.0_10\bin
+
+::PATH = %PATH%;C:\Program Files (x86)\ImageMagick-6.7.3-Q16
+PATH = %PATH%;C:\Windows\system32
+PATH = %PATH%;c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin
+PATH = %PATH%;c:\Program Files (x86)\MSBuild\14.0\Bin
+PATH = %PATH%;c:\Qt\5.6\msvc2015\bin
+PATH = %PATH%;c:\Qt\Tools\QtCreator\bin
+
+::PATH = %PATH%;c:\Program Files (x86)\7-zip
+PATH = %PATH%;c:\Program Files (x86)\php
+::PATH = %PATH%;%HOMEDRIVE%%HOMEPATH%\php5
+::PATH = %PATH%;C:\Program Files (x86)\MySQL\MySQL Server 5.5\bin
+::PATH = %PATH%;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64
+::PATH = %PATH%;C:\Program Files (x86)\gnuplot\bin
+::PATH = %PATH%;%HOMEDRIVE%%HOMEPATH%\adt-bundle-windows\sdk\tools
+::PATH = %PATH%;%HOMEDRIVE%%HOMEPATH%\adt-bundle-windows\sdk\platform-tools
+::PATH = %PATH%;%HOMEDRIVE%%HOMEPATH%\apache-ant-1.8.4\bin
+::PATH = %PATH%;c:\Program Files\Java\jdk1.8.0_25\bin
+::PATH = %PATH%;C:\Program Files (x86)\Notepad++
+
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
+:done
+
